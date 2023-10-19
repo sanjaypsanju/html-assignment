fbb21ccdc7736d1c3f1738c4c8378eb307e1d668
Sanjay P <129385155+sanjaypsanju@users.noreply.github.com>
2023-10-19 10:47:15 -0600

    first commit

---
+++ python_math/math.py
@@ -0,0 +1,22 @@
+import math
+
+#celi value
+print("celisius value",math.ceil(3.70))
+
+#floor vale
+print("floor value",math.floor(-5.12))
+
+#fab value
+print("fab value",math.fabs(9))
+
+#factorial of a number
+print("factorial of 8",math.factorial(8))
+
+#gcd value
+print("gdcd value of 4,12",math.gcd(4,12))
+
+#square root
+print("square root of 4 is",math.sqrt(4))
+
+#value of pi
+print("value of pi ",math.pi)
